48a08d3066984e0e2d6c5e3e0894f48abe3e160a
Di <elven.legacy@hotmail.com>
2021-04-10 23:51:17 +0300

    Изменение пароля и автоматический вход. Оптимизация graphql

--- src/pages/change-password/[token].tsx
+++ src/pages/change-password/[token].tsx
@@ -4,25 +4,41 @@ import {Form, Formik} from "formik";
 import {toErrorMap} from "../../util/toErrorMap";
 import {Box, Button} from "@chakra-ui/react";
 import InputField from "../../components/inputField";
-import React from "react";
+import React, {useState} from "react";
 import {useRouter} from "next/router";
+import {useChangePasswordMutation} from "../../generated/graphql";
+import {withUrqlClient} from "next-urql";
+import {CreateUrqlClient} from "../../util/createUrqlClient";
 
 const ChangePassword: NextPage<{token: string}> = ({token}) => {
     const router = useRouter();
+    const [,changePassword] = useChangePasswordMutation();
+    const [fatalError, setFatalError] = useState('');
     return (<Wrapper variant="small">
         <Formik
             initialValues={{newPassword: "", newPasswordConfirmed: ""}}
             onSubmit={async (values,{setErrors}) => {
-                const response = await login(values);
-                if (response.data?.login.errors) {
-                    setErrors(toErrorMap(response.data.login.errors));
-                } else if (response.data?.login.user){
-                    await router.push('/login');
+                const response = await changePassword({
+                    ...values,
+                    token
+                });
+                if (response.data?.changePassword.errors) {
+                    const errorMap = toErrorMap(response.data.changePassword.errors);
+                    if ('fatalError' in errorMap){
+                        setFatalError(errorMap.fatalError);
+                    }
+                    setErrors(errorMap);
+                } else if (response.data?.changePassword.user){
+                    await router.push('/');
                 }
             }}
         >
             {({values, handleChange, isSubmitting}) => (
                 <Form>
+                    {fatalError.length > 0 && <Box color='red'>
+                        {fatalError}
+                    </Box>
+                    }
                     <Box><InputField name="newPassword" label="enter new password" placeholder="new password" type="password"/></Box>
                     <Box mt={4}><InputField name="newPasswordConfirmed" label="enter new password again" placeholder="new password again" type="password"/></Box>
                     <Box mt={4}><Button isLoading={isSubmitting} type="submit">login</Button></Box>
@@ -38,4 +54,6 @@ ChangePassword.getInitialProps = ({query}) =>{
     }
 }
 
-export default ChangePassword;
+export default withUrqlClient(
+    CreateUrqlClient
+)(ChangePassword);
